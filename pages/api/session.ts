b4429865a7738eca8babaa52f6a9d57197247e54
Vampo7152 <umangboss786@gmail.com>
2023-02-20 20:17:46 +0000

    stuff

--- pages/api/session.ts
+++ pages/api/session.ts
@@ -17,13 +17,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             quantity: 1,
           },
         ],
-        discounts: {
-          collection_id:
-            "ac64ea74280411e2e145dadd18c2d5b6",
-          discount: 0.5,
-          name: "Rolling Bones",
-          image: "https://cdn.coralcube.io/111/rs:fit:400:400:0:0/aHR0cHM6Ly9uZnQtY29sbGFnZS5zMy5ldS1jZW50cmFsLTEuYW1hem9uYXdzLmNvbS9wdWJsaWMvcm9sbGluZ19ib25lcy5QTkc_dD0xNjc2NDgwNzAw",
-        },
       });
 
       return res.status(200).json(response);
